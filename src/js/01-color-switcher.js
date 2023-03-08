a9ec4fe65934defb6b6b3e204be36db34896e5d6
ToFFeeV <khmiluvkiyv.ku@gmail.com>
2023-03-08 19:05:43 +0200

    Task-01 done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,45 @@
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
 
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
+const body = document.body;
+let timerId = null;
+
+startBtn.addEventListener("click", () => {
+    startBtn.disabled = true;
+    stopBtn.disabled = false;
+
+    timerId = setInterval(() => {
+        body.style.backgroundColor = getRandomHexColor();
+    }, 1000);
+});
+
+stopBtn.addEventListener("click", () => {
+    startBtn.disabled = false;
+    stopBtn.disabled = true;
+
+    clearInterval(timerId);
+});
+
+// Version 2
+
+// startBtn.addEventListener("click", startRandomHexColor);
+// stopBtn.addEventListener("click", stopRandomHexColor);
+
+// function startRandomHexColor() {
+//     startBtn.disabled = true;
+//     stopBtn.disabled = false;
+
+//     timerId = setInterval(() => {
+//         body.style.backgroundColor = getRandomHexColor();
+//     }, 1000);
+// };
+
+// function stopRandomHexColor() {
+//     startBtn.disabled = false;
+//     stopBtn.disabled = true;
+
+//     clearInterval(timerId);
+// };
